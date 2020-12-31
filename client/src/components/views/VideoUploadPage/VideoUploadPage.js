83e2ba4cbb3bbe8409732dddf38a19a8b8d72301
hansol <kimhan0421@naver.com>
2020-12-31 11:52:01 +0900

    modify prettier

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,101 +1,104 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
-import { Typography, Form, Input, Icon } from 'antd'
-import { PlayCircleOutlined } from '@ant-design/icons'
-
+import { Typography, Form, Input, Icon } from 'antd';
+import { PlayCircleOutlined } from '@ant-design/icons';
 
 const { Title } = Typography;
 const { TextArea } = Input;
 
 const PrivateOptions = [
-    { value: 0, label: 'Private' },
-    { value: 1, label: 'Public' }
-]
+  { value: 0, label: 'Private' },
+  { value: 1, label: 'Public' },
+];
 
 const CategoryOption = [
-    { value: 0, label: "Film & Animation" },
-    { value: 1, label: "Autos & Vehicles" },
-    { value: 2, label: "Music" },
-    { value: 3, label: "Pets & Animals" },
-    { value: 4, label: "Sports" },
-]
+  { value: 0, label: 'Film & Animation' },
+  { value: 1, label: 'Autos & Vehicles' },
+  { value: 2, label: 'Music' },
+  { value: 3, label: 'Pets & Animals' },
+  { value: 4, label: 'Sports' },
+];
 
 function VideoUploadPage() {
-    const [VideoTitle, setVideoTitle] = useState("")
-    const [Description, setDescription] = useState("")
-    const [Private, setPrivate] = useState(0) // 1
-    const [Category, setCategory] = useState("Film & Animation")
+  const [VideoTitle, setVideoTitle] = useState('');
+  const [Description, setDescription] = useState('');
+  const [Private, setPrivate] = useState(0); // 1
+  const [Category, setCategory] = useState('Film & Animation');
 
-    const handleChangeTitle = (event) => {
-        setVideoTitle(event.currentTarget.value)
-    }
+  const handleChangeTitle = (event) => {
+    setVideoTitle(event.currentTarget.value);
+  };
 
-    const handleChangeDescription = (event) => {
-        setDescription(event.currentTarget.value)
-    }
+  const handleChangeDescription = (event) => {
+    setDescription(event.currentTarget.value);
+  };
 
-    const handleChangePrivate = (event) => {
-        setPrivate(event.currentTarget.value)
-    }
+  const handleChangePrivate = (event) => {
+    setPrivate(event.currentTarget.value);
+  };
 
-    const handleChangesetCategory = (event) => {
-        setCategory(event.currentTarget.value)
-    }
-    return (
-        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
-            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
-                <Title level={2} style={{ textAlign: 'center' }}>
-                    <PlayCircleOutlined />&nbsp; Upload Video
-                </Title>
-            </div>
-
-            <Form >
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Dropzone
-                        onDrop
-                        multiple
-                        maxSize>
-                        {({ getRootProps, getInputProps }) => (
-                            <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-                                {...getRootProps()}
-                            >
-                                <input {...getInputProps()} />
-                                <Icon type="plus" style={{ fontSize: '3rem' }} />
-
-                            </div>
-                        )}
-                    </Dropzone>
-                </div>
-            </Form>
-            <br /> <br />
-            <label>Title</label>
-            <Input
-                onChange={handleChangeTitle}
-                value={VideoTitle}
-            />
-
-            <br /><br />
-            <label>Description</label>
-            <TextArea
-                onChange={handleChangeDescription}
-                value={Description}
-            />
-            <br /><br />
-            <select onChange={handleChangePrivate}>
-                {PrivateOptions.map((item, index) => (
-                    <option key={index} value={item.value}>{item.label}</option>
-                ))}
-            </select>
-            <br /><br />
-
-            <select onChange={handleChangesetCategory}>
-                {CategoryOption.map((item, index) => (
-                    <option key={index} value={item.label}>{item.label}</option>
-                ))}
-            </select>
-            <br /><br />
+  const handleChangesetCategory = (event) => {
+    setCategory(event.currentTarget.value);
+  };
+  return (
+    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        <Title level={2} style={{ textAlign: 'center' }}>
+          <PlayCircleOutlined />
+          &nbsp; Upload Video
+        </Title>
+      </div>
+      <Form>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Dropzone onDrop multiple maxSize>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                  width: '300px',
+                  height: '240px',
+                  border: '1px solid lightgray',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                <Icon type="plus" style={{ fontSize: '3rem' }} />
+              </div>
+            )}
+          </Dropzone>
         </div>
-    )
+      </Form>
+      <br /> <br />
+      <label>Title</label>
+      <Input onChange={handleChangeTitle} value={VideoTitle} />
+      <br />
+      <br />
+      <label>Description</label>
+      <TextArea onChange={handleChangeDescription} value={Description} />
+      <br />
+      <br />
+      <select onChange={handleChangePrivate}>
+        {PrivateOptions.map((item, index) => (
+          <option key={index} value={item.value}>
+            {item.label}
+          </option>
+        ))}
+      </select>
+      <br />
+      <br />
+      <select onChange={handleChangesetCategory}>
+        {CategoryOption.map((item, index) => (
+          <option key={index} value={item.label}>
+            {item.label}
+          </option>
+        ))}
+      </select>
+      <br />
+      <br />
+    </div>
+  );
 }
 
-export default VideoUploadPage
+export default VideoUploadPage;
